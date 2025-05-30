3536dd2171aa7e9e3456eb0c24eeb20dc92a7bf4
festafantastica <festafantastica.decoracoes@gmail.com>
2025-05-30 20:27:03 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // 👈 Isso resolve o problema dos assets na Hostinger!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react']
