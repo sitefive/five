2e4070c055a9578e66808f15e38280b64ddc9d1b
festafantastica <festafantastica.decoracoes@gmail.com>
2025-05-30 23:55:26 -0300

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
             <h3 className="text-xl font-semibold text-white mb-4">{t('footer.quickLinks')}</h3>
             <ul className="space-y-2">
               <li>
-                <Link to="/#expert" className="hover:text-white transition-colors duration-300">
+                <Link to="/#experts" className="hover:text-white transition-colors duration-300">
                   {t('menu.about')}
                 </Link>
               </li>
