bc75e99794c232941b4b855b85594448599fd98c
festafantastica <festafantastica.decoracoes@gmail.com>
2025-06-01 13:30:06 -0300

    Fix Footer privacy and terms links

--- src/pages/TermsOfUse.tsx
+++ src/pages/TermsOfUse.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import ParallaxHeader from '../components/ParallaxHeader';
 
 const TermsOfUse = () => {
