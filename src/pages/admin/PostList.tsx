c5e3dc7e99e45097020ae06d5fd59278f8694f7e
festafantastica <festafantastica.decoracoes@gmail.com>
2025-06-20 09:11:41 -0300

    Updated PostList.tsx

--- src/pages/admin/PostList.tsx
+++ src/pages/admin/PostList.tsx
@@ -199,7 +199,7 @@ const PostList = () => {
             <thead className="bg-gray-50">
               <tr>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  {t('common.title_label')}
+                  {t('post.title_label')}
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   {t('post.author_label')}
