fc568ee9237e01b82e8f5acfd25119324114c3d3
Five Consulting <fiveconsulting@fiveconsulting.com.br>
2025-07-13 14:45:17 -0300

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -100,7 +100,7 @@ const Contact = () => {
                   <Mail className="w-5 h-5 mr-2" />
                   <h3 className="font-medium text-lg">Email</h3>
                 </div>
-                <p className="text-gray-600">fiveconsulting@fiveconsulting.com.br</p>
+                <p className="text-gray-600">contato@fiveconsulting.com.br</p>
               </div>
             </div>
           </div>
